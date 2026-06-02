ee88a7b2e9b26c60e484b3af0877c252e2174f55
mmathss <matiarevalo611@gmail.com>
2026-06-02 13:13:28 -0500

    fix: adaptador Netlify para deploy estatico

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import keystatic from '@keystatic/astro';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), keystatic()],
-  output: 'hybrid', // Requerido para servir la interfaz del CMS dinámicamente
+  integrations: [],
+  output: 'static',
+  adapter: netlify(),
   vite: {
     server: {
       allowedHosts: true
