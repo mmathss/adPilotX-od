9b8df53c476d70d22492ca867d78b415c7008031
Diego Matias <matiarevalo611@gmail.com>
2026-06-03 12:38:15 -0500

    chore: retornar a modo estatico (SSG) para hosting gratuito ilimitado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
-  output: 'server',
-  adapter: netlify(),
+  output: 'static',
   vite: {
     server: {
       allowedHosts: true
